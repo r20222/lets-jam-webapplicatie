206f36bf96aa36b1fe3fba19512d1d0a10729c27
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2024-01-18 14:56:46 +0100

    #13 #20 #22
    #20 Map: Zoom-in-out button
    
    #22 Map: Hint voor gebruiker (zoom-in effect)
    
    #13 map opsplitsen volgens atomic design

---
+++ src/lib/atoms/Link.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let link
+	export let href
+</script>
+
+<a {href}>{link}</a>
+
+<style>
+	a {
+		color: var(--darkBlue);
+		padding: 0.4em;
+		text-decoration: none;
+	}
+
+	a:focus {
+		outline-color: var(--darkBlue);
+	}
+
+	a:hover {
+		border-bottom: 1px solid var(--darkBlue);
+	}
+</style>
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,2 +1,10 @@
 // place files you want to import through the `$lib` alias in this folder.
-export {default as statusColor} from './atoms/statusColor.svelte'
+export { default as DropDown } from '$lib/atoms/DropDown.svelte'
+export { default as Link } from '$lib/atoms/Link.svelte'
+
+export { default as MapDropDown } from  '$lib/molecules/MapDrownDown.svelte'
+
+export { default as Map } from '$lib/organisms/Map.svelte'
+
+
+
