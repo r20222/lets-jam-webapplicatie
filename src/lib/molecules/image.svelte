52cc2b0f84d4a1e647c9ad8142d2b286fcfe46d1
Cathelijne Visser <cathelijne.visser@live.nl>
2024-01-18 09:37:47 +0100

    small changes

--- src/lib/molecules/image.svelte
+++ src/lib/molecules/image.svelte
@@ -16,7 +16,8 @@
     })
 </script>
 <section>
-        <button class="control control--left" title="carrousel scroll left">
+        <button class="control control--left" >
+            <title>scroll left</title>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M15.293 3.293 6.586 12l8.707 8.707 1.414-1.414L9.414 12l7.293-7.293-1.414-1.414z"/></svg>
         </button>
         <ul class="scroller">
@@ -29,7 +30,8 @@
                 </li>
             {/each}
         </ul>
-        <button class="control control--right" title="carrousel scroll right">
+        <button class="control control--right">
+            <title>scroll right</title>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M7.293 4.707 14.586 12l-7.293 7.293 1.414 1.414L17.414 12 8.707 3.293 7.293 4.707z"/></svg>
         </button>
 </section>
