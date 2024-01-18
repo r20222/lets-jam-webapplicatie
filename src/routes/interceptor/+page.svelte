053d837a3179077b5a865c9136bf0e0dcc67e702
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2024-01-18 14:59:00 +0100

    #13

--- src/routes/interceptor/+page.svelte
+++ src/routes/interceptor/+page.svelte
@@ -1,84 +1,116 @@
 <script>
 	import Navigation from '../../lib/components/navigation.svelte';
-    import Infotext from '../../lib/components/info-text.svelte';
+	import Infotext from '../../lib/components/info-text.svelte';
 	import TrashRemoved from '../../lib/components/trash-removed.svelte';
-    import Startdate from '../../lib/components/startdate.svelte';
-    import Image from '../../lib/molecules/image.svelte';
-    import Map from '../../lib/components/map.svelte'
-    export let data
-    let gridWithWithoutNav;
+	import Startdate from '../../lib/components/startdate.svelte';
+	import Image from '../../lib/molecules/image.svelte';
+	import { Map } from '$lib/index.js';
+	import {
+		initializeMap,
+		createRiverGeoData,
+		createOceanGeoData,
+		addMapLoadEvent,
+		addClickEvent,
+		addMouseEnterEvent,
+		addMouseLeaveEvent
+	} from '../../lib/utils/mapUtils.js';
+	export let data;
+	let gridWithWithoutNav;
+	import { onMount } from 'svelte';
+	let map;
+
+	onMount(() => {
+		map = initializeMap();
+
+		const riverGeoData = createRiverGeoData(data);
+		const oceanGeoData = createOceanGeoData(data);
+
+		addMapLoadEvent(map, oceanGeoData, riverGeoData);
+
+		addClickEvent(map, (properties, coordinates) => {
+			if (riverGeoData) {
+				window.location.href = `/interceptor/?id=${properties.id}&coordinates=${JSON.stringify(
+					coordinates
+				)}`;
+			} else {
+				window.location.href = `/oceanSystem?coordinates=${JSON.stringify(coordinates)}`;
+			}
+		});
+		addMouseEnterEvent(map);
+		addMouseLeaveEvent(map);
+	});
 </script>
 
 <svelte:head>
-    <title>{data.currentInterceptorHygraph[0].titlePage}</title>
+	<title>{data.currentInterceptorHygraph[0].titlePage}</title>
 </svelte:head>
 
-<Navigation bind:visible={gridWithWithoutNav} data={data.dataHygraph.dashboard.nav}/>
-<section class="{gridWithWithoutNav ? 'margin-with-navigation' : 'margin-without-navigation'}">
-    <h2>{data.currentInterceptor[0].name}</h2>
-    <Infotext data={data.currentInterceptorHygraph[0].riverInfoText} />
-    <TrashRemoved data={data.currentInterceptor[0]} text={data.dataHygraph} />
-    <Startdate data={data.currentInterceptor[0]} text={data.currentInterceptorHygraph[0].riverStartDate} />
-    <Image data={data.currentInterceptorHygraph[0].riverImage} />
-    <div class="map">
-        <Map data={data} />
-    </div>
+<Navigation bind:visible={gridWithWithoutNav} data={data.dataHygraph.dashboard.nav} />
+<section class={gridWithWithoutNav ? 'margin-with-navigation' : 'margin-without-navigation'}>
+	<h2>{data.currentInterceptor[0].name}</h2>
+	<Infotext data={data.currentInterceptorHygraph[0].riverInfoText} />
+	<TrashRemoved data={data.currentInterceptor[0]} text={data.dataHygraph} />
+	<Startdate
+		data={data.currentInterceptor[0]}
+		text={data.currentInterceptorHygraph[0].riverStartDate}
+	/>
+	<Image data={data.currentInterceptorHygraph[0].riverImage} />
+	<div class="map">
+		<Map {data} />
+	</div>
 </section>
 
 <!-- Scroll to top button -->
 <a href="#top" class="scroll-top" aria-label="scroll to top">
 	<!-- add icon -->{data.dataHygraph.dashboard.buttonTop}
 </a>
-   
-<style>
 
-    /* Als darkmode de standaard instelling is */
-    @media (prefers-color-scheme: dark) {
-        :root {
-        --darkBlue: #ffffff;
-        --lightBlue: #5CC8DE;
-        --whiteColor: #143653;
-        --lightGray: #0D2437;
-        --accentGray: rgb(228, 228, 228);
-        --textColor: #ffffff;
-        --boxShadow: rgba(128, 128, 128, 0.0);
-        --color: rgb(212, 212, 212);
-        --textSize: 1.2rem;
-        --iconSize: 2rem;
-        --trashRemovedBackground: #143653;
-        } 
-    }
-    .map {
+<style>
+	/* Als darkmode de standaard instelling is */
+	@media (prefers-color-scheme: dark) {
+		:root {
+			--darkBlue: #ffffff;
+			--lightBlue: #5cc8de;
+			--whiteColor: #143653;
+			--lightGray: #0d2437;
+			--accentGray: rgb(228, 228, 228);
+			--textColor: #ffffff;
+			--boxShadow: rgba(128, 128, 128, 0);
+			--color: rgb(212, 212, 212);
+			--textSize: 1.2rem;
+			--iconSize: 2rem;
+			--trashRemovedBackground: #143653;
+		}
+	}
+	.map {
 		grid-area: map;
 		border-radius: 0.5rem;
 		padding: 0.5rem;
 		background-color: var(--whiteColor);
 		box-shadow: var(--boxShadow) 0px 0px 8px;
 		transition: 0.2s;
-        margin: 0;
-	} 
-    /* mobiele versie */
-    section{
-     margin: 5rem 1.5rem 1.5rem 1.5rem;
-     display: grid;
-     grid-template-columns: 1fr, 1fr;
-     grid-template-rows: 0.1fr, 0.3fr, 6fr, 2fr, 1fr, 1fr, 1fr, 1fr;
-     gap: 1.2rem;
-     grid-template-areas:
-         "header header"
-         "trash-removed-total trash-removed-last-month"
-         "dashboard-info dashboard-info"
-         "dashboard-info-startdate dashboard-info-startdate"
-         "map map"
-         "image image"
-         
-     }
- 
-     h2 {
-         grid-area: header;
-     }
+		margin: 0;
+	}
+	/* mobiele versie */
+	section {
+		margin: 5rem 1.5rem 1.5rem 1.5rem;
+		display: grid;
+		grid-template-columns: 1fr, 1fr;
+		grid-template-rows: 0.1fr, 0.3fr, 6fr, 2fr, 1fr, 1fr, 1fr, 1fr;
+		gap: 1.2rem;
+		grid-template-areas:
+			'header header'
+			'trash-removed-total trash-removed-last-month'
+			'dashboard-info dashboard-info'
+			'dashboard-info-startdate dashboard-info-startdate'
+			'map map'
+			'image image';
+	}
 
-     
+	h2 {
+		grid-area: header;
+	}
+
 	/* Scroll to top */
 	.scroll-top {
 		position: absolute;
@@ -96,30 +128,30 @@
 		text-transform: uppercase;
 	}
 
-     /* vanaf 992px */
-     @media (min-width:62em) {
-     section{
-         margin: 0rem 2rem 2rem 18rem;
-         grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-         /* grid-template-rows: 0.01fr 0.1fr 0.3fr 0.4fr 0.4fr 0.6fr; */
-         grid-template-areas:
-         "header header header header header"
-            "trash-removed-total trash-removed-last-month map map map"
-            "dashboard-info dashboard-info map map map"
-            "dashboard-info dashboard-info map map map"
-            "dashboard-info dashboard-info map map map"
-            "dashboard-info-startdate dashboard-info-startdate image image image"
-            "dashboard-info-startdate dashboard-info-startdate image image image"
-            "dashboard-info-startdate dashboard-info-startdate image image image";
-     }
-       /* past de margin aan als de nav in of uitgeklapt is */
-		.margin-with-navigation{
+	/* vanaf 992px */
+	@media (min-width: 62em) {
+		section {
 			margin: 0rem 2rem 2rem 18rem;
-			transition:.1s;
+			grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+			/* grid-template-rows: 0.01fr 0.1fr 0.3fr 0.4fr 0.4fr 0.6fr; */
+			grid-template-areas:
+				'header header header header header'
+				'trash-removed-total trash-removed-last-month map map map'
+				'dashboard-info dashboard-info map map map'
+				'dashboard-info dashboard-info map map map'
+				'dashboard-info dashboard-info map map map'
+				'dashboard-info-startdate dashboard-info-startdate image image image'
+				'dashboard-info-startdate dashboard-info-startdate image image image'
+				'dashboard-info-startdate dashboard-info-startdate image image image';
 		}
-		.margin-without-navigation{
+		/* past de margin aan als de nav in of uitgeklapt is */
+		.margin-with-navigation {
+			margin: 0rem 2rem 2rem 18rem;
+			transition: 0.1s;
+		}
+		.margin-without-navigation {
 			margin: 0rem 2rem 2rem 2rem;
-			transition:.1s;
+			transition: 0.1s;
 		}
- }   
+	}
 </style>
--- src/routes/oceanSystem/+page.svelte
+++ src/routes/oceanSystem/+page.svelte
@@ -1,31 +1,67 @@
 <script>
 	import Navigation from '../../lib/components/navigation.svelte';
-    import Infotext from '../../lib/components/info-text.svelte';
-    import TrashRemovedOcean from '../../lib/components/trash-removed.svelte';
-    import LastPortCall from '../../lib/components/lastportcall.svelte'
-    import Image from '../../lib/molecules/image.svelte';
+	import Infotext from '../../lib/components/info-text.svelte';
+	import TrashRemovedOcean from '../../lib/components/trash-removed.svelte';
+	import LastPortCall from '../../lib/components/lastportcall.svelte';
+	import Image from '../../lib/molecules/image.svelte';
 	import Startdate from '../../lib/components/startdate.svelte';
-    import Map from '../../lib/components/map.svelte'
-    export let data
-    let gridWithWithoutNav;
+	import { Map, MapDropDown } from '$lib/index.js';
+	import {
+		initializeMap,
+		RiverGeoData,
+		OceanGeoData,
+		addMapLoadEvent,
+		addClickEvent,
+		addMouseEnterEvent,
+		addMouseLeaveEvent
+	} from '../../lib/utils/mapUtils.js';
+	export let data;
+	let gridWithWithoutNav;
+
+	import { onMount } from 'svelte';
+	let map;
+
+	onMount(() => {
+		map = initializeMap();
+
+		const riverGeoData = RiverGeoData(data);
+		const oceanGeoData = OceanGeoData(data);
+
+		addMapLoadEvent(map, oceanGeoData, riverGeoData);
+
+		addClickEvent(map, (properties, coordinates) => {
+			if (riverGeoData) {
+				window.location.href = `/interceptor/?id=${properties.id}&coordinates=${JSON.stringify(
+					coordinates
+				)}`;
+			} else {
+				window.location.href = `/oceanSystem?coordinates=${JSON.stringify(coordinates)}`;
+			}
+		});
+		addMouseEnterEvent(map);
+		addMouseLeaveEvent(map);
+	});
 </script>
 
 <svelte:head>
-    <title>{data.dataHygraph.dashboard.ocean.titlePage}</title>
+	<title>{data.dataHygraph.dashboard.ocean.titlePage}</title>
 </svelte:head>
 
-    <Navigation  bind:visible={gridWithWithoutNav} data={data.dataHygraph.dashboard.nav}/>
-    
-<section class="{gridWithWithoutNav ? 'margin-with-navigation' : 'margin-without-navigation'}">
-    <h2>{data.dataApi.systems[0]. name}</h2>
-    <TrashRemovedOcean data={data.dataApi.systems[0]} text={data.dataHygraph} />
-    <Infotext data={data.dataHygraph.dashboard.ocean.oceanInfotext} />
-    <Startdate data={data.dataApi.systems[0]} text={data.dataHygraph.dashboard.ocean.oceanStartDate} />
-    <LastPortCall data={data} />
-    <Image data={data.dataHygraph.dashboard.ocean.oceanImage} />
-    <div class="map">
-        <Map data={data} />
-    </div>
+<Navigation bind:visible={gridWithWithoutNav} data={data.dataHygraph.dashboard.nav} />
+
+<section class={gridWithWithoutNav ? 'margin-with-navigation' : 'margin-without-navigation'}>
+	<h2>{data.dataApi.systems[0].name}</h2>
+	<TrashRemovedOcean data={data.dataApi.systems[0]} text={data.dataHygraph} />
+	<Infotext data={data.dataHygraph.dashboard.ocean.oceanInfotext} />
+	<Startdate
+		data={data.dataApi.systems[0]}
+		text={data.dataHygraph.dashboard.ocean.oceanStartDate}
+	/>
+	<LastPortCall {data} />
+	<Image data={data.dataHygraph.dashboard.ocean.oceanImage} />
+	<section class="map">
+		<Map {data} />
+	</section>
 </section>
 
 <!-- Scroll to top button -->
@@ -33,72 +69,71 @@
 	<!-- add icon -->{data.dataHygraph.dashboard.buttonTop}
 </a>
 
-
 <style>
-        :root {
-            --darkBlue: #143653;
-            --lightBlue: #5CC8DE;
-            --whiteColor: #ffffff;
-            --lightGray: #f7f7f7;
-            --accentGray: rgb(228, 228, 228);
-            --textColor: #143653;
-            --boxShadow: rgba(128, 128, 128, 0.132);
-            --color: rgb(212, 212, 212);
-            --textSize: 1.2rem;
-            --iconSize: 2rem;
-            --trashRemovedBackground: white;
-        }
-            
-        /* Als darkmode de standaard instelling is */
-        @media (prefers-color-scheme: dark) {
-        :root {
-        --darkBlue: #ffffff;
-        --lightBlue: #5CC8DE;
-        --whiteColor: #143653;
-        --lightGray: #0D2437;
-        --accentGray: rgb(228, 228, 228);
-        --textColor: #ffffff;
-        --boxShadow: rgba(128, 128, 128, 0.0);
-        --color: rgb(212, 212, 212);
-        --textSize: 1.2rem;
-        --iconSize: 2rem;
-        --trashRemovedBackground: #143653;
-        } 
-  }
-  
-  .map {
+	:root {
+		--darkBlue: #143653;
+		--lightBlue: #5cc8de;
+		--whiteColor: #ffffff;
+		--lightGray: #f7f7f7;
+		--accentGray: rgb(228, 228, 228);
+		--textColor: #143653;
+		--boxShadow: rgba(128, 128, 128, 0.132);
+		--color: rgb(212, 212, 212);
+		--textSize: 1.2rem;
+		--iconSize: 2rem;
+		--trashRemovedBackground: white;
+	}
+
+	/* Als darkmode de standaard instelling is */
+	@media (prefers-color-scheme: dark) {
+		:root {
+			--darkBlue: #ffffff;
+			--lightBlue: #5cc8de;
+			--whiteColor: #143653;
+			--lightGray: #0d2437;
+			--accentGray: rgb(228, 228, 228);
+			--textColor: #ffffff;
+			--boxShadow: rgba(128, 128, 128, 0);
+			--color: rgb(212, 212, 212);
+			--textSize: 1.2rem;
+			--iconSize: 2rem;
+			--trashRemovedBackground: #143653;
+		}
+	}
+
+	.map {
 		grid-area: map;
 		border-radius: 0.5rem;
 		padding: 0.5rem;
 		background-color: var(--whiteColor);
 		box-shadow: var(--boxShadow) 0px 0px 8px;
 		transition: 0.2s;
-        margin: 0;
+		margin: 0;
 	}
-    /* Grid container */
-    /* soms staat de grid area al gedefinieerd in de component zelf */
+	/* Grid container */
+	/* soms staat de grid area al gedefinieerd in de component zelf */
 
-    /* mobiele versie */
-   section{
-    margin: 5rem 1.5rem 1.5rem 1.5rem;
-    display: grid;
-    grid-template-columns: 1fr, 1fr;
-    /* grid-template-rows: 0.1fr, 0.3fr, 6fr, 2fr, 1fr, 1fr, 1fr, 1fr; */
-    gap: 1.2rem;
-    grid-template-areas:
-        "header header"
-        "trash-removed-total trash-removed-last-month"
-        "dashboard-info dashboard-info"
-        "dashboard-info-startdate dashboard-info-startdate"
-        "dashboard-info-lastportcall dashboard-info-lastportcall"
-        "map map"
-        "image image"
-    }
+	/* mobiele versie */
+	section {
+		margin: 5rem 1.5rem 1.5rem 1.5rem;
+		display: grid;
+		grid-template-columns: 1fr, 1fr;
+		/* grid-template-rows: 0.1fr, 0.3fr, 6fr, 2fr, 1fr, 1fr, 1fr, 1fr; */
+		gap: 1.2rem;
+		grid-template-areas:
+			'header header'
+			'trash-removed-total trash-removed-last-month'
+			'dashboard-info dashboard-info'
+			'dashboard-info-startdate dashboard-info-startdate'
+			'dashboard-info-lastportcall dashboard-info-lastportcall'
+			'map map'
+			'image image';
+	}
 
-    h2 {
-        grid-area: header;
-    }
-    
+	h2 {
+		grid-area: header;
+	}
+
 	/* Scroll to top */
 	.scroll-top {
 		position: absolute;
@@ -116,31 +151,30 @@
 		text-transform: uppercase;
 	}
 
-    /* vanaf 992px */
-    @media (min-width:62em) {
-    section{
-        /* margin: 0rem 2rem 2rem 18rem; */
-        grid-template-columns: repeat(5, 1fr);
-        /* grid-template-rows: 0.01fr 0.1fr 0.3fr 0.4fr 0.4fr 0.6fr; */
-        grid-template-areas:
-            "header header header header header"
-            "trash-removed-total trash-removed-last-month map map map"
-            "dashboard-info dashboard-info map map map"
-            "dashboard-info dashboard-info map map map"
-            "dashboard-info dashboard-info map map map"
-            "dashboard-info-startdate dashboard-info-startdate image image image"
-            "dashboard-info-lastportcall dashboard-info-lastportcall image image image"
-            "dashboard-info-lastportcall dashboard-info-lastportcall image image image";
-    }
-    /* past de margin aan als de nav in of uitgeklapt is */
-		.margin-with-navigation{
+	/* vanaf 992px */
+	@media (min-width: 62em) {
+		section {
+			/* margin: 0rem 2rem 2rem 18rem; */
+			grid-template-columns: repeat(5, 1fr);
+			/* grid-template-rows: 0.01fr 0.1fr 0.3fr 0.4fr 0.4fr 0.6fr; */
+			grid-template-areas:
+				'header header header header header'
+				'trash-removed-total trash-removed-last-month map map map'
+				'dashboard-info dashboard-info map map map'
+				'dashboard-info dashboard-info map map map'
+				'dashboard-info dashboard-info map map map'
+				'dashboard-info-startdate dashboard-info-startdate image image image'
+				'dashboard-info-lastportcall dashboard-info-lastportcall image image image'
+				'dashboard-info-lastportcall dashboard-info-lastportcall image image image';
+		}
+		/* past de margin aan als de nav in of uitgeklapt is */
+		.margin-with-navigation {
 			margin: 0rem 2rem 2rem 18rem;
-			transition:.1s;
+			transition: 0.1s;
 		}
-		.margin-without-navigation{
+		.margin-without-navigation {
 			margin: 0rem 2rem 2rem 2rem;
-			transition:.1s;
+			transition: 0.1s;
 		}
-}
-    
+	}
 </style>
