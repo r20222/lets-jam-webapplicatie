ba3cdbebd592cf2bfef70e92e276bd1774273ce8
Cathelijne Visser <cathelijne.visser@live.nl>
2024-01-19 12:15:16 +0100

    index.js linkt goed door

--- src/lib/molecules/MapDropDown.svelte
+++ src/lib/molecules/MapDropDown.svelte
@@ -1,6 +1,6 @@
 <script>
 	export let data
-	import { DropDown, Link } from '../lib/index.js'
+	import { DropDown, Link } from '../index.js'
 
 	const getStatusColor = (status) => {
 		switch (status) {
