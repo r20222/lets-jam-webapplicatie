ba93ea26cf8b47da390a49759ae9f38082c629c6
Cathelijne Visser <cathelijne.visser@live.nl>
2024-01-19 13:37:03 +0100

    svelte2tsx installed

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,10 +1,9 @@
 // place files you want to import through the `$lib` alias in this folder.
-import DropDown from '../lib/atoms/DropDown.svelte'
-import Link from '../lib/atoms/Link.svelte'
+import DropDown from './atoms/DropDown.svelte'
+import Link from './atoms/Link.svelte'
 
-import MapDropDown from '../lib/molecules/MapDropDown.svelte'
+import MapDropDown from './molecules/MapDropDown.svelte'
 
 import Map from './organisms/map.svelte'
-
 
 export { Map, MapDropDown, Link, DropDown }
