eb48a15c60019adebcef4093b54ba35a361d0a84
Cathelijne Visser <cathelijne.visser@live.nl>
2024-01-19 12:12:31 +0100

    default as deleted

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,10 +1,10 @@
 // place files you want to import through the `$lib` alias in this folder.
-import { default as DropDown } from '../lib/atoms/DropDown.svelte'
-import { default as Link } from '../lib/atoms/Link.svelte'
+import DropDown from '../lib/atoms/DropDown.svelte'
+import Link from '../lib/atoms/Link.svelte'
 
-import { default as MapDropDown } from '../lib/molecules/MapDropDown.svelte'
+import MapDropDown from '../lib/molecules/MapDropDown.svelte'
 
-import { default as Map } from './organisms/map.svelte'
+import Map from './organisms/map.svelte'
 
 
 export { Map, MapDropDown, Link, DropDown }
